0790cde44a7f36dc9ec9a535cecb3af760dcce3e
Promise337 <promiseanazodo123@gmail.com>
2023-02-18 10:18:23 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,14 +2,14 @@
 
 
 /**
- *main - print "Programming is like building a multilingual puzzle, followed by a newline.
- *Return: Always 0
+ * main - print "Programming is like building a multilingual puzzle, followed by a newline.
+ * Return: Always 0
  */
 
 
 int main(void) 
 
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 } 
