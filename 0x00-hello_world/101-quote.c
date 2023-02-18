ed3d5ff19ba9a7252e03fe00b94235b4f53ac60b
Promise337 <promiseanazodo123@gmail.com>
2023-02-18 11:48:02 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+
+
+/**main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19,
+ *  followed by a new line, to the standard error.
+ *  Return; Alaways 1
+ */
+
+
+int main(void)
+
+{
+	write(2, "and that place of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return(1);
+}
