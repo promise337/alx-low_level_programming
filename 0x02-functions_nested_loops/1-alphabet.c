2fa5fcd21c558e08bae2920a7e45c14de9f1ce95
Promise337 <promiseanazodo123@gmail.com>
2023-02-22 05:01:29 -0800

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+
+/** 
+ * print_alphabet - Make the alphabet(void)
+ * Return: void
+ */
+
+void print_alphabet(void)
+
+{
+
+	char c;
+	for(c = 'a'; c <= '2'; c++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
+
+}
