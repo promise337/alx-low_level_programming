27b7301421b819c5008c35102f90eb20fed01a08
Promise337 <promiseanazodo123@gmail.com>
2023-02-22 10:38:22 -0800

    10-add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+
+/**
+ * @n1: the first num to be computed
+ * @n2: the second num to be computed
+ * Return: integer value
+ */
+
+int add(int n1, int n2)
+
+{
+	return (n1 + n2);
+
+}
