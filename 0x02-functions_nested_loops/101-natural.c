728fc10c1ae52987584cd0621a2ebbab1c1ea464
Promise337 <promiseanazodo123@gmail.com>
2023-02-22 11:57:16 -0800

    101-natutral

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+
+/**
+ * main - prints the sum of multiples of 3 and 5 till 1024
+ * Return: Always (Success)
+ */
+
+int main(void)
+
+{
+	int i, z = 0;
+
+	while (i < 1024)
+	{
+	if ((i % 3 == 0) || (i % 5 == 0))
+	{
+        z += i;
+	}
+	i++;
+	}
+	printf("%d\n", z);
+	return (0);
+
+}
