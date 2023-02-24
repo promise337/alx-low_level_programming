350cc7e645f6464ad42cdad252233a48943e7974
Promise337 <promiseanazodo123@gmail.com>
2023-02-24 04:42:47 -0800

    0-isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isupper - checks if a letter is uppercase
+ * @x: the number to be checked
+ * Return: 1 for upper letter or 0 for any else
+ */
+
+int _isupper.c(int x)
+
+{
+
+	if (x >= 65 && x <= 90)
+	{
+	return (1);
+        }
+	return (0);
+
+}
