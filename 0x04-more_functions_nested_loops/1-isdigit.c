5d46da672652b540c6303ac5aa6b904e6efa37ca
Promise337 <promiseanazodo123@gmail.com>
2023-02-24 05:01:10 -0800

    2-mul

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * isdigit - check if a character is a digit
+ * @x: the number to be checked
+ * Return: 1 for digit or 0 for any else
+ */
+
+
+int _isdigit(int x)
+
+{
+
+	if (x >= 48 && x <= 57)
+	{
+	return (1);
+	}
+	return (0);
+
+}
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+
+/**
+ * mul - multiplies two integers
+ * @x: the first number to multiply
+ * @y: the second number to multiply the first
+ * Return: the result of the multiplication
+ */
+
+int mul(int x, int y)
+
+{
+	return (x * y)
+}
