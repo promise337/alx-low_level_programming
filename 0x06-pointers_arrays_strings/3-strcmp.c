5ab3183f366fa20d8c41ae9d480b9c25ccc5215d
Promise337 <promiseanazodo123@gmail.com>
2023-03-02 03:09:07 -0800

    3-strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares pointers to two strings
+ * @s1: a pointer to the first string compared
+ * @s2: a pointer to the second string compared
+ * Return: 0
+ */
+
+ int _strcmp(char *s1, char *s2)
+
+{
+	while (*s1 && *s2 && *s1 == *s2)
+	{
+	s1++;
+	s2++;
+	}
+	return (*s1 - *s2);
+
+}
+
